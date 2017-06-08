9e425b7efb7aeaca59aa647db997ce600dbd6dff
scumworth <danmcninch@gmail.com>
2017-06-07 20:08:10 -0400

    updated index

--- utils/request-handler.js
+++ utils/request-handler.js
@@ -2,11 +2,9 @@ const getUserObj = (req) => {
     userObj = {};
     const rawIp = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress;
-    console.log(req.headers['x-forwarded-for']);
     const rawLang = req.headers["accept-language"];
     const rawOs = req.headers["user-agent"]
     
-    console.log(req.headers);
     //parse ip address
     if (rawIp.indexOf(':') !== -1) {
         userObj.ipaddress = rawIp.split(':').pop();
